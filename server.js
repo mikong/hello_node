541d7b01aac0ccc7ecc0b8532d7dac119ee59483
Michael Galero <michaelgalero@gmail.com>
2013-11-25 18:17:42 +0800

    Change port to 6000 (default EY).

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
-}).listen(3000, '127.0.0.1');
+}).listen(6000, '127.0.0.1');
