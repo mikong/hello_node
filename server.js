b805f90542cb04d56dee9a32ddc867a6a0d4ed05
Mikong Galero <mgalero@knights-mbp.local>
2013-11-29 13:56:28 +0800

    Add a console.log to test logging.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  console.log('Saying hello...');
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.js\n');
 }).listen(6000, '127.0.0.1');
